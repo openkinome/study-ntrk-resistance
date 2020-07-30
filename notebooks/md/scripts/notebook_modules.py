3816350912990476f2925edc475a66f86bd7c292
William Glass <wglass149@googlemail.com>
2020-07-30 18:46:26 +0100

    update md notebook and modules

--- notebooks/md/scripts/notebook_modules.py
+++ notebooks/md/scripts/notebook_modules.py
@@ -1,19 +1,17 @@
 import MDAnalysis as mda
 import MDAnalysis.analysis.rms
 import MDAnalysis.transformations as trans
+import matplotlib.patheffects as mpe
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
+# Check here for good CB safe palettes: https://venngage.com/blog/color-blind-friendly-palette/
 cb_colour_cycle = [
-    "#377eb8",
-    "#ff7f00",
-    "#4daf4a",
-    "#f781bf",
-    "#a65628",
-    "#984ea3",
-    "#999999",
-    "#e41a1c",
-    "#dede00",
+    (245, 105, 58),  # orange
+    (169, 90, 161),  # purple
+    (133, 192, 249),  # light blue
 ]
 
 
@@ -38,7 +36,7 @@ def centre_protein(dict_of_systs, wrap=False, cent="geometry"):
     return dict_of_systs
 
 
-def calc_rmsd(name, dict_of_systs):
+def calc_rmsd(name, dict_of_systs, skip=10):
 
     rmsd_store = []
 
@@ -64,24 +62,31 @@ def calc_rmsd(name, dict_of_systs):
         # "backbone and resid 648-650" # HRD motif
         # "backbone and resid 668-694" # Activation loop
 
-        R.run()
+        R.run(step=skip)
         rmsd_store.append(R)
 
     # Plot the RMSDs
+
     fig = plt.figure(figsize=(6, 6))
     ax = fig.add_subplot(111)
 
+    time = rmsd_store[0].rmsd.T[1] / 1000
+
     for i, syst in enumerate(dict_of_systs):
 
+        col = tuple(round(c / 255, 2) for c in cb_colour_cycle[i])
+
         rmsd = rmsd_store[i].rmsd.T  # transpose makes it easier for plotting
-        time = rmsd[1] / 1000
-        ax.plot(time, rmsd[2], cb_colour_cycle[i], label=name + ":" + syst, alpha=0.8)
-        ax.legend(loc="best")
-        ax.set_xlabel("Time (ns)")
-        ax.set_ylabel(r"RMSD ($\AA$)")
+        ax.plot(
+            time, rmsd[2], color=col, label=name + ":" + syst, alpha=0.8
+        )
+
+    ax.legend(loc="best")
+    ax.set_xlabel("Time (ns)", fontsize=14)
+    ax.set_ylabel(r"RMSD ($\AA$)", fontsize=14)
 
 
-def delta_cog_inhib(name, dict_of_systs):
+def delta_cog_inhib(name, dict_of_systs, skip=None, hist=False, save=False, **kwargs):
 
     # Calculate the distance changes between the original docked ligand's centre of geometry
     # and the centre of geometry at each frame
@@ -103,7 +108,7 @@ def delta_cog_inhib(name, dict_of_systs):
         inhib_sel = u.select_atoms(sel)
         ref_cog = inhib_sel.center_of_geometry()
 
-        for ts in u.trajectory:
+        for ts in u.trajectory[::skip]:
 
             inhib_store[syst]["time"].append(ts.time / 1000)
 
@@ -114,16 +119,34 @@ def delta_cog_inhib(name, dict_of_systs):
     # Plot
     fig = plt.figure(figsize=(6, 6))
     ax = fig.add_subplot(111)
+    ax.set_xlim([kwargs["xmin"], kwargs["xmax"]])
+    ax.set_ylim([kwargs["ymin"], kwargs["ymax"]])
 
     i = 0
 
     for sel, syst in zip(inhib_resnames, dict_of_systs):
 
+        col = tuple(round(c / 255, 2) for c in cb_colour_cycle[i])
+
         cog = inhib_store[syst]["cog"]
         time = inhib_store[syst]["time"]
-        ax.plot(time, cog, cb_colour_cycle[i], label=name + ":" + syst, alpha=0.8)
-        ax.legend(loc="best")
-        ax.set_xlabel("Time (ns)")
-        ax.set_ylabel(r"$\Delta$ Distance ($\AA$)")
+
+        if hist:
+            ax.hist(cog, density=True, bins=100, color=col, label=name + ":" + syst, alpha=0.8)
+        else:
+            ax.plot(time, cog, color=col, label=name + ":" + syst, alpha=0.8)
 
         i += 1
+
+    ax.legend(loc="best")
+
+    if hist:
+        ax.set_xlabel(r"$\Delta$ Distance ($\AA$)", fontsize=14)
+        ax.set_ylabel("Density", fontsize=14)
+
+    else:
+        ax.set_xlabel("Time (ns)", fontsize=14)
+        ax.set_ylabel(r"$\Delta$ Distance ($\AA$)", fontsize=14)
+
+    if save:
+        plt.savefig("inhib_cog_plot.png", format="png")

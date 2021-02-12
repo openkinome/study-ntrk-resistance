201d4a4cbe9dfcefb30fb1bfce40c3ae4da9863c
William Glass <wglass149@googlemail.com>
2021-02-12 06:42:14 -0500

    allow undefined stereo in HTF setup

--- setup_scripts/ntrk_mutation_htf_setup.py
+++ setup_scripts/ntrk_mutation_htf_setup.py
@@ -15,7 +15,7 @@ from perses.utils.openeye import generate_unique_atom_names
 from simtk import unit
 
 parser = argparse.ArgumentParser(
-    description="Prepare hybrid topologies for NTRK mutant systems"
+    description="Prepare hybrid topologies for NTRK mutant systems with flattened torsions"
 )
 parser.add_argument(
     "--index", dest="index", type=int, help="an integer for the system to create"
@@ -27,16 +27,22 @@ parser.add_argument(
     help="the path of where to write out the files",
 )
 parser.add_argument(
-    "--csv_data", dest="csv_data", type=str, help="the path to the CSV data e.g. /path/to/csv_data/"
+    "--csv_data",
+    dest="csv_data",
+    type=str,
+    help="the path to the CSV data e.g. /path/to/csv_data/",
 )
 parser.add_argument(
-    "--data_path", dest="data_path", type=str, help="the path to both PDB and SDF data e.g. /path/to/data/"
+    "--data_path",
+    dest="data_path",
+    type=str,
+    help="the path to both PDB and SDF data e.g. /path/to/data/",
 )
 parser.add_argument(
     "--small_mol_ff",
     dest="small_mol_ff",
     type=str,
-    default="openff-1.2.0",
+    default="openff-1.3.0",
     help="the forcefield to use for the small molecule",
 )
 args = parser.parse_args()
@@ -93,12 +99,13 @@ df_i = df[df["ntrk"] == index_dict[i][0]]
 data_path = args.data_path
 out_path = args.output_path
 
-# check mutations output directory exists
-out_dir = f"{out_path}mutations_htf_output/"
+# check mutations output directory doesn't exist
+out_dir = f"{out_path}mutations_htf_output/flattened_torsions/"
+print(f"Using {out_path} as the output directory...")
 if not os.path.exists(out_dir):
-    sys.exit(
-        f"{out_path}mutations_htf_output/ not found! Please create this directory and retry."
-    )
+    os.makedirs(out_dir)
+else:
+    sys.exit(f"{out_path}mutations_htf_output/ already exists! Exiting.")
 
 # set three letter to one letter amino acid dictionary
 aa = {
@@ -124,50 +131,57 @@ aa = {
     "MET": "M",
 }
 
-htf_input_prefix = 'kinoml_OEKLIFSKinaseHybridDockingFeaturizer_'#NTRK1_larotrectinib_complex'
+htf_input_prefix = "kinoml_OEKLIFSKinaseHybridDockingFeaturizer_"
 
 # make the systems
 for index, row in df_i.iterrows():
 
     print(row)
 
-    ligand_name = row['tki']
-    protein_name = row['ntrk']
+    ligand_name = row["tki"]
+    protein_name = row["ntrk"]
     resid = str(
         row["resid"]
     )  # The resid has to be a string for PointMutationExecutor input
     mutate_from = row["from"]
     mutate_to = row["to"]
 
-    print("info:", protein_name, ligand_name, mutate_from, resid, mutate_to)
+    print(f"info: {protein_name}, {ligand_name}, {mutate_from}, {resid}, {mutate_to}")
 
     # Create protein directory
-    output_prefix_protein = f"{out_path}mutations_htf_output/{protein_name}"
+    output_prefix_protein = (
+        f"{out_path}mutations_htf_output/flattened_torsions/{protein_name}"
+    )
     if not os.path.exists(output_prefix_protein):
         os.makedirs(output_prefix_protein)
         print(f"--> Directory {output_prefix_protein} created")
 
     # Create protein mutant directory
-    output_prefix_mutant = f"{out_path}mutations_htf_output/{protein_name}/{aa[mutate_from]}{resid}{aa[mutate_to]}"
+    output_prefix_mutant = f"{out_path}mutations_htf_output/flattened_torsions/{protein_name}/{aa[mutate_from]}{resid}{aa[mutate_to]}"
     if not os.path.exists(output_prefix_mutant):
         os.makedirs(output_prefix_mutant)
         print(f"--> Directory {output_prefix_mutant} created")
-    
-    protein_file = f"{data_path}{htf_input_prefix}{protein_name}_{ligand_name}_protein.pdb"
-    ligand_file = f"{data_path}{htf_input_prefix}{protein_name}_{ligand_name}_ligand.sdf"
+
+    protein_file = (
+        f"{data_path}{htf_input_prefix}{protein_name}_{ligand_name}_protein.pdb"
+    )
+    ligand_file = (
+        f"{data_path}{htf_input_prefix}{protein_name}_{ligand_name}_ligand.sdf"
+    )
+    print(f"Using protein file: {protein_file}")
     print(f"Using ligand file: {ligand_file}")
 
-    # create oemol object for NTRK ligands due to issues with stereochemistry
-    ifs = oechem.oemolistream()
-    ifs.open(ligand_file)
-    mol_list = [oechem.OEMol(mol) for mol in ifs.GetOEMols()]
-    mol = mol_list[0]
-    if len([atom.GetName() for atom in mol.GetAtoms()]) > len(set([atom.GetName() for atom in mol.GetAtoms()])):
-            mol = generate_unique_atom_names(mol)
-    oechem.OEAssignAromaticFlags(mol, oechem.OEAroModelOpenEye)
-    oechem.OEAssignHybridization(mol)
-    oechem.OEAddExplicitHydrogens(mol)
-    oechem.OEPerceiveChiral(mol)
+    # create oemol object for NTRK ligands due to issues with stereochemistry - DEPRECATED
+    # ifs = oechem.oemolistream()
+    # ifs.open(ligand_file)
+    # mol_list = [oechem.OEMol(mol) for mol in ifs.GetOEMols()]
+    # mol = mol_list[0]
+    # if len([atom.GetName() for atom in mol.GetAtoms()]) > len(set([atom.GetName() for atom in mol.GetAtoms()])):
+    #         mol = generate_unique_atom_names(mol)
+    # oechem.OEAssignAromaticFlags(mol, oechem.OEAroModelOpenEye)
+    # oechem.OEAssignHybridization(mol)
+    # oechem.OEAddExplicitHydrogens(mol)
+    # oechem.OEPerceiveChiral(mol)
 
     # make the system
     solvent_delivery = PointMutationExecutor(
@@ -175,9 +189,13 @@ for index, row in df_i.iterrows():
         "1",  # First and only protein chain
         resid,
         mutate_to,
-        ligand_input=mol,
+        ligand_file=ligand_file,
+        allow_undefined_stereo=True,
         ionic_strength=0.15 * unit.molar,
         small_molecule_forcefields=args.small_mol_ff,
+        flatten_torsions=True,  # we're using flattened torsions
+        flatten_exceptions=True,
+        conduct_endstate_validation=False,
     )
 
     # make image map of the transformation
